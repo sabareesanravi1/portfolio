86de9e0e7e558ed503768766b64b594e6712ac4e
Sabareesan R <sabaresan-10041@csez.zohocorpin.com>
2023-02-17 13:19:44 +0530

    Iniital react files has been added to the repository.

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="ocean">
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+    </div>
+  );
+}
+
+export default App;
